9f404ca3e486d38b47d65a6706c9f3d5e53dc848
agustinfranchetti <agustinfranchetti@gmail.com>
2022-07-06 17:54:46 -0300

    Update settings.py

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "cmf_database",
         "USER": os.getenv("MYSQL_USER"),
-        "PASSWORD": "123123",
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
